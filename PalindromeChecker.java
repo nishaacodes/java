8593fd91c20472bdb818fc17c416db4ae5128ff8
nishaacodes <mnishanthini06@gmail.com>
2026-07-06 11:38:23 +0530

    name change

---
+++ PalindromeChecker.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static boolean isPalindrome(String text) {
+        text = text.replaceAll("\\s+", "").toLowerCase();
+
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word or sentence: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
